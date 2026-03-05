8a004cfc2110d2b4f8ff3cf8c4696bb5fefec995
neoxolotl <fernanditososa@gmail.com>
2026-03-05 23:09:01 +0000

    icon

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'mi-blog-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  'https://cdn.jsdelivr.net/npm/marked/marked.min.js'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activar Service Worker
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar requests
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
